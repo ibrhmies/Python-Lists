80ee1c4e6f5171eb6d5f6dad0cd77030ab5451be
ibrhmies <02220201044@ogr.inonu.edu.tr>
2022-10-31 17:39:11 +0300

    Python-Lists
    Python'da liste özellikleri

---
+++ lists.py
@@ -0,0 +1,16 @@
+msg = "Python uygulamalarıma hoş geldiniz.Ben İbrahim SEVEN."
+
+sayilar = [1,2,3,4,5]
+
+#sonuc = sayilar -> sayilar karakter dizisindeki bütün terimler görünür.
+#sonuc = sayilar[0] -> sayilar karakter dizisindeki 0. terim görünür.
+#sonuc = sayilar[1] -> sayilar karakter dizisindeki 1. terim görünür.
+#sonuc = sayilar[5] -> IndexError: list index out of range sorunu gelir çünkü 5. terim yoktur.
+
+isimler = ['Ahmet','Ayşe','Mehmet']
+
+sonuc = isimler[0]
+
+print(sonuc)
+
+
